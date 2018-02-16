1cd31658c8acc5fd99a4c0e4a0557152f786d765
Maxii194 <36174642+Maxii194@users.noreply.github.com>
2018-02-15 22:15:02 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,7 +1,7 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
-	return Math.random();
+	var numeroRandom = Math.floor(Math.random()*10)+1 ;
+	alert(numeroRandom);
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,23 @@
 function Mostrar()
 {
 //tomo el mes
-var mesDelAño = document.getElementById('mes').value;
+    var mesDelAño = document.getElementById('mes').value;
 
+    switch(mesDelAño){
 
+        case 'Enero':
+            alert("Que comiences bien el año!!!");
+            break;
+        case 'Marzo':
+            alert("A clases!!!");
+            break; 
+        case 'Julio':
+            alert("Se vienen las vacaciones!!!");
+            break;
+        case 'Diciembre':
+            alert("Felices Fiesta!!!");
+            break;        
+    }
 
 
 }//FIN DE LA FUNCIÓN
